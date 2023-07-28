30bb73a552a0905064d8dcf9293f0bc4b3615524
Fabien Péan <fabien@pean.pro>
2023-07-28 22:02:38 +0000

    Update parpack seed -- non-zero

--- PARPACK/SRC/MPI/pdgetv0.f
+++ PARPACK/SRC/MPI/pdgetv0.f
@@ -234,7 +234,7 @@ c
          call MPI_COMM_RANK(comm, myid, ierr)
          iseed(1) = 1
          iseed(2) = 2
-         iseed(3) = mod(myid,2039)
+         iseed(3) = 1+mod(myid,2039)
          iseed(4) = 1+2*mod(myid,2048)
          inits = .false.
       end if
